ca8fd02a3ea0d1f3aeb638a4233be56a7accfafc
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-07-23 08:40:07 +0300

    modified:   css/main.css 	modified:   css/main.css.map 	modified:   css/main.min.css 	modified:   css/main.min.css.map 	modified:   images/icons.svg 	modified:   index.html 	new file:   js/menu.js 	modified:   portfolio.html 	modified:   sass/components/_card-set.scss 	modified:   sass/components/_footer.scss 	modified:   sass/components/_image-set.scss 	modified:   sass/components/_subscribe.scss 	modified:   sass/components/header/_contacts.scss 	modified:   sass/components/header/_container.scss 	modified:   sass/components/header/_header.scss 	modified:   sass/components/header/_logo.scss 	modified:   sass/components/header/_navigation.scss 	modified:   sass/utils/_common.scss

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    togglerMenu: document.querySelector("[data-menu-button]"),
+    Menu: document.querySelector("[data-menu]")
+  };
+
+  refs.togglerMenu.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.togglerMenu.classList.toggle("is-open");
+    refs.Menu.classList.toggle("visually-hidden");
+    document.body.classList.toggle("off_overflow");
+  }
+})();
